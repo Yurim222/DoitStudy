f75997d379213f7c4e98e587a8a5a03f876ef400
신 우현 <woo_hyun_1@naver.com>
2023-03-28 10:42:49 +0900

    1. Learn about basic concept about react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const ToDoList = () => {
+  return (
+    <div style={{ border: "1px solid black", width: "200px" }}>
+      <h1>My Book</h1>
+      <ul>
+        <li>Study</li>
+        <li>Exercise</li>
+        <li>Read book</li>
+      </ul>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <di style={{ display: "flex" }}>
+      {/* <ToDoList />
+      <ToDoList />
+      <ToDoList /> */}
+      <ToDoList />
+      <ToDoList />
+      <ToDoList />
+    </di>
   );
 }
 
